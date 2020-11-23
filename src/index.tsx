730a85cb3292772bc3525b32c2d9aeb919142648
ohmink <kkop5056@naver.com>
2020-11-23 16:39:31 +0900

    chore: sentry 삭제 sentry 코드를 삭제하였다.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
+import Login from '@pages/Login';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <Route path="/main" component={App} exact />
+    <Route path="/" component={Login} exact />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
