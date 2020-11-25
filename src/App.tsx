0ff070f4df297a711a2d5503a8543aab815d5c63
WooYoo <puba5@naver.com>
2020-11-25 15:41:39 +0900

    feat : 메인페이지 좌측 네비게이션 바 생성
    메인 페이지의 좌측 네비게이션 바 생성
    기본적으로 생성되어 있던 의미 없는 내용들 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
 import React from 'react';
+import NavBar from './components/main/NavBar';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar></NavBar>
     </div>
   );
 };
---
+++ src/components/main/NavBar.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const NavBar = () => {
+  return <Wrapper></Wrapper>;
+};
+
+const Wrapper = styled.p`
+  position: fixed;
+  margin: 0;
+  left: 0;
+  top: 0;
+  width: 300px;
+  height: 100vh;
+  background: blue;
+`;
+
+export default NavBar;
