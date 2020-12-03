8894484c6ccd6d93e0374f289f8d50107a0801e3
JeesooHaa <haajeesoo@gmail.com>
2020-12-04 01:27:26 +0900

    fix: Route 위치 변경
     Route의 위치를 index에서 App으로 변경했습니다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import Projects from './pages/Projects';
-import Login from './pages/Login';
-import GlobalStyle from './style/globalStyle';
+import { Route } from 'react-router-dom';
+import Main from '@pages/Main';
+import Login from '@pages/Login';
+import GlobalStyle from '../style/globalStyle';
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
+    <>
       <GlobalStyle />
-      <Route path="/projects" component={Projects} exact />
+      <Route path="/main" component={Main} exact />
       <Route path="/" component={Login} exact />
-    </BrowserRouter>
+    </>
   );
 };
 
