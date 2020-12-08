499752faace6c5ec14d7cc2b89b12f099c8f2505
JeesooHaa <haajeesoo@gmail.com>
2020-12-09 03:22:24 +0900

    feat: ProjectCard 컴포넌트 추가
     Project 페이지의 project 정보를 표현할 ProjectCard 컴포넌트를
     추가했습니다.
     클릭시 해당 프로젝트의 이슈페이지로 이동하도록 했습니다.

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
+import ProjectCard from '@components/projects/ProjectCard';
 import Api from '@utils/Api';
 
 const ProjectsRoot = styled.div`
   top: 4.75rem;
   margin: 2rem;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  column-gap: 2rem;
+  row-gap: 2rem;
 `;
 
 const Projects = () => {
-  const history = useHistory();
-
   const [projects, setProjects] = useState([]);
 
   const readProjects = async () => {
@@ -22,7 +24,13 @@ const Projects = () => {
     readProjects();
   }, []);
 
-  return <ProjectsRoot>test</ProjectsRoot>;
+  return (
+    <ProjectsRoot>
+      {projects.map(project => (
+        <ProjectCard key={project._id} project={project} />
+      ))}
+    </ProjectsRoot>
+  );
 };
 
 export default Projects;
