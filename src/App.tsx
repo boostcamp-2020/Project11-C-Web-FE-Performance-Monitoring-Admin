d868147070940793c39b4a4ce4f2a009b2ebaf5e
ohmink <kkop5056@naver.com>
2020-12-09 10:44:04 +0900

    chore: import 변경 폴더 변경에 따른 경로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import Projects from './pages/Projects';
+import { Route } from 'react-router-dom';
+import Issues from './pages/Issues';
 import Login from './pages/Login';
 import GlobalStyle from './style/globalStyle';
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
+    <div>
       <GlobalStyle />
-      <Route path="/projects" component={Projects} exact />
+      <Route path="/main" component={Issues} exact />
       <Route path="/" component={Login} exact />
-    </BrowserRouter>
+    </div>
   );
 };
 
