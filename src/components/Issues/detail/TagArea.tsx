81b4165d3c697a7cbe9cc821b65ae697bd355646
Dongmin Shin <nsdm821@gmail.com>
2020-12-10 15:52:45 +0900

    feat: tag 출력 컴포넌트 skeleton code issue.tags 출력만 하도록 구현

---
+++ src/components/Issues/detail/TagArea.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const TagContainer = styled.div`
+  border: 0.05rem rgba(0, 0, 0, 0.35) solid;
+`;
+
+const TagArea = (props: { tags: {} }) => {
+  const { tags } = props;
+
+  const tagArr = Object.keys(tags).map(key => [key, tags[key]]);
+  return (
+    <TagContainer>
+      {tagArr.map(tag => (
+        <div>
+          <span>{`${tag[0]} `}</span>
+          <span>{tag[1]}</span>
+        </div>
+      ))}
+    </TagContainer>
+  );
+};
+
+export default TagArea;
