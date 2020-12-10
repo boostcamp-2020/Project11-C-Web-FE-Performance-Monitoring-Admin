b5809822205610b640be9f2a9310c13cd44d94ea
ohmink <kkop5056@naver.com>
2020-12-10 17:46:26 +0900

    feat: 이슈 목록 화면의 기능 구현 체크박스, Resolve, 담당자 등의 추가 화면 및 기능 구현

--- src/components/Issues/issue/Filter.tsx
+++ src/components/Issues/issue/Filter.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import axios from 'axios';
 import SortButton from './SortButton';
 import SearchButton from './SearchButton';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -19,13 +21,26 @@ const useStyles = makeStyles(theme => ({
     color: 'white',
     fontSize: '1.2rem',
   },
-  projects: {
+  project: {
+    display: 'flex',
+    flexDirection: 'row',
     gridColumnEnd: 'span 3',
     margin: '0 auto',
-    marginTop: '1.2rem',
+    marginTop: '1rem',
     marginBottom: '1rem',
     color: 'white',
     fontSize: '1.2rem',
+  },
+  projectName: {
+    paddingTop: '0.5rem',
+    paddingBottom: '0.5rem',
+    fontSize: '0.8rem',
+  },
+  projectNameValue: {
+    color: 'white',
+    textTransform: 'none',
+    fontSize: '1.3rem',
+    marginLeft: '0.5rem',
   },
   column: {
     gridColumnEnd: 'span 2',
@@ -39,10 +54,40 @@ const useStyles = makeStyles(theme => ({
 
 const Filter = props => {
   const classes = useStyles();
+  const [projectName, setProjectName] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const getProject = async () => {
+      try {
+        setProjectName(null);
+
+        const response: any = await axios.get(
+          `http://localhost:3000/project/${props.projectId}`,
+          {
+            withCredentials: true,
+          }
+        );
+        console.log(response.data);
+        setProjectName(response.data.title);
+      } catch (e) {
+        setError(e);
+      }
+    };
+
+    getProject();
+  }, []);
+
+  if (error) return <div>에러가 발생했습니다</div>;
+  if (!projectName) return null;
+
   return (
     <div className={classes.container}>
       <div className={classes.issueCount}>Issues ({props.eventNum})</div>
-      <div className={classes.projects}>Project Name</div>
+      <div className={classes.project}>
+        <span className={classes.projectName}>Project Title : </span>
+        <p className={classes.projectNameValue}>{projectName}</p>
+      </div>
       <div className={classes.column}>
         <SortButton />
       </div>
--- src/components/Issues/issue/TableColumn.tsx
+++ src/components/Issues/issue/TableColumn.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import DoneIcon from '@material-ui/icons/Done';
+import axios from 'axios';
+import IssueItem from './IssueItem';
+import { ResolveDispatchContext, ResolveStateContext } from './ResolveProvider';
 
 const useStyles = makeStyles(theme => ({
   container: {
     display: 'grid',
     gridTemplateColumns: 'repeat(12, 1fr)',
     gridGap: theme.spacing(3),
-    backgroundColor: 'rgba(189,189,189,0.3)',
+    backgroundColor: 'rgba(89,89,89,0.3)',
   },
   controller: {
     gridColumnEnd: 'span 5',
@@ -54,24 +57,124 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-const TableColumn = () => {
+const TableColumn = props => {
   const classes = useStyles();
+  const resolveDispatch = useContext(ResolveDispatchContext);
+  const resolveState = useContext(ResolveStateContext);
+  const { issues } = props;
+  const [disable, setDisable] = useState(true);
+  console.log('밖', resolveState);
+
+  const handleCheckedAll = event => {
+    console.log('all before', resolveState);
+
+    const itemCheckBox = document.querySelectorAll('.item_checkbox');
+    itemCheckBox.forEach(
+      checkbox =>
+        ((checkbox as HTMLInputElement).checked = !(checkbox as HTMLInputElement)
+          .checked)
+    );
+
+    if (event.target.checked) {
+      issues.forEach(issue => {
+        resolveDispatch({ type: 'add', issueId: issue._id });
+      });
+    } else {
+      resolveDispatch({ type: 'removeAll' });
+    }
+
+    console.log('all result', resolveState);
+
+    if (disable === true) {
+      setDisable(false);
+    } else {
+      setDisable(true);
+    }
+  };
+
+  const handleChecked = event => {
+    console.log('before', resolveState);
+
+    const checkbox = event.target;
+
+    if (checkbox.checked) {
+      if (!resolveState.includes(checkbox.id)) {
+        resolveDispatch({ type: 'add', issueId: checkbox.id });
+      }
+    } else if (resolveState.includes(checkbox.id)) {
+      console.log('해제');
+      resolveDispatch({ type: 'remove', issueId: checkbox.id });
+    }
+
+    if (resolveState.length === 0) {
+      setDisable(false);
+    } else {
+      setDisable(true);
+    }
+
+    console.log('after', resolveState);
+  };
+
+  const handleResolve = async () => {
+    // 서버로 이슈들의 상태를 전송해야함.
+    const updateIssueResolve = async () => {
+      const response = await axios;
+    };
+
+    console.log('resolve button test');
+    console.log(resolveState);
+  };
+
+  useEffect(() => {
+    if (resolveState.length === 0) {
+      console.log('useEffect');
+      setDisable(true);
+    }
+    return () => {
+      console.log('컴포넌트가 화면에서 사라짐');
+    };
+  }, []);
+
   return (
-    <div className={classes.container}>
-      <div className={classes.controller}>
-        <input type="checkbox" className={classes.controllerItem} />
-        <Button
-          variant="contained"
-          className={classes.resolveButton}
-          startIcon={<DoneIcon />}
-          disabled={true}
-        >
-          Resolve
-        </Button>
+    <div>
+      <div className={classes.container}>
+        <div className={classes.controller}>
+          <input
+            type="checkbox"
+            className={classes.controllerItem}
+            onClick={handleCheckedAll}
+          />
+          <Button
+            variant="contained"
+            className={classes.resolveButton}
+            startIcon={<DoneIcon />}
+            id="Resolved_button"
+            disabled={disable}
+            onClick={handleResolve}
+          >
+            Resolve
+          </Button>
+        </div>
+        <div className={classes.graph}>Graph</div>
+        <div className={classes.column}>Events</div>
+        <div className={classes.column}>Assigned</div>
       </div>
-      <div className={classes.graph}>Graph</div>
-      <div className={classes.column}>Events</div>
-      <div className={classes.column}>Assigned</div>
+      {issues.map(issue => (
+        <IssueItem
+          key={issue._id}
+          issueId={issue._id}
+          name={issue.name}
+          description={issue.message}
+          eventNum={issue.errorEvents.length}
+          userNum={issue.users}
+          assigned={issue.assigned}
+          errorEvents={issue.errorEvents}
+          stack={issue.stack}
+          date={issue.createdAt}
+          projectId={issue.projectId}
+          onClick={handleChecked}
+        />
+      ))}
     </div>
   );
 };
