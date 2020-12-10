ebe2588d98e79335f0d0639e0cb92cad6e8dfb28
ohmink <kkop5056@naver.com>
2020-12-10 19:12:45 +0900

    fix: 에러 코드 표기를 주석 처리 서버 프로젝트는 각 에러의 코드를 저장하지만 브라우저는 저장하지 않는다. 따라서 서버와 브라우저를 구분하여 처리해야한다. 일단 주석 처리

--- src/components/Issues/detail/TagArea.tsx
+++ src/components/Issues/detail/TagArea.tsx
@@ -7,12 +7,12 @@ const TagContainer = styled.div`
 
 const TagArea = (props: { tags: {} }) => {
   const { tags } = props;
-
   const tagArr = Object.keys(tags).map(key => [key, tags[key]]);
+
   return (
     <TagContainer>
       {tagArr.map(tag => (
-        <div>
+        <div key={tag[0]}>
           <span>{`${tag[0]} `}</span>
           <span>{tag[1]}</span>
         </div>
