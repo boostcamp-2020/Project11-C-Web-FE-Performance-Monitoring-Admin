45625c7b11ca75f0b17b5f6e797c476e6e240620
ohmink <kkop5056@naver.com>
2020-12-11 11:39:43 +0900

    feat: 전역 헤더 추가

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,24 @@ import Issues from '@pages/Issues';
 import IssueDetail from '@pages/IssueDetail';
 import GlobalStyle from './style/globalStyle';
 import Header from './components/utils/Header';
+import AppBar from '@material-ui/core/AppBar';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    backgroundColor: 'rgba(0,0,0,0.3)',
+  },
+}));
 
 const App: React.FC = () => {
+  const classes = useStyles();
   return (
     <>
       <GlobalStyle />
-      <Header />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Header />
+      </AppBar>
       <Route path="/" component={Login} exact />
       <Route path="/projects" component={Projects} exact />
       <Switch>
