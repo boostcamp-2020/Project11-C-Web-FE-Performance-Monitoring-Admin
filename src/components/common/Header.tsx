0551bc2584df0cf7b0f813e01f56f728a0e2227a
ohmink <kkop5056@naver.com>
2020-12-15 01:12:09 +0900

    refactor: 파일 이동 utils에서 common으로 이동

---
+++ src/components/common/Header.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import styled from 'styled-components';
+import Grid from '@material-ui/core/Grid';
+
+const HeaderButton = styled.button`
+  margin: 0 auto;
+  padding: 1rem;
+  padding-left: 2rem;
+
+  font-size: 1.5rem;
+  color: white;
+
+  text-align: center;
+  background-color: rgba(255, 255, 255, 0);
+
+  &:hover {
+    color: lightgray;
+  }
+`;
+
+const HomeButton = styled.button`
+  margin: 0 auto;
+  padding: 1rem;
+  padding-left: 2rem;
+
+  font-size: 1.5rem;
+  color: #64ffda;
+
+  text-align: center;
+  background-color: rgba(255, 255, 255, 0);
+
+  &:hover {
+    color: #00bfa5;
+  }
+`;
+
+const HeaderContainer = styled.div`
+  background-color: rgba(0, 0, 0, 0.3);
+`;
+
+const GlobalHeader = () => {
+  const HomeHandler = () => {
+    //process.env로 변경해야 함.
+    window.location.href = 'http://localhost:8000/';
+  };
+  return (
+    <HeaderContainer>
+      <Grid container spacing={1}>
+        <Grid item xs>
+          <HomeButton onClick={HomeHandler}>@Acent</HomeButton>
+        </Grid>
+        <Grid item xs={6}>
+          <HeaderButton>Docs</HeaderButton>
+          <HeaderButton>Tutorial</HeaderButton>
+        </Grid>
+        <Grid item xs>
+          <HeaderButton>Sign in</HeaderButton>
+          <HeaderButton>Sign up</HeaderButton>
+        </Grid>
+      </Grid>
+    </HeaderContainer>
+  );
+};
+
+export default GlobalHeader;
