582fe46f497330479eacba3c843596497315365b
puba.yu <puba@pubayuucbookpro.lan>
2020-12-14 13:47:14 +0900

    feat : 이메일 정규표현식 변수명 수정
    regExp에서 ㄷemailRegExp로 수정

--- src/components/login/RightContents.tsx
+++ src/components/login/RightContents.tsx
@@ -99,8 +99,8 @@ const RightContents = () => {
   };
 
   const isRightEmail = inputValue => {
-    const regExp = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]+\.[a-zA-Z]+$/i;
-    return regExp.test(inputValue);
+    const emailRegExp = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]+\.[a-zA-Z]+$/i;
+    return emailRegExp.test(inputValue);
   };
 
   const isRightPassword = inputValue => {
