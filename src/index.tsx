a1cfb3d6efe50f62a2f31a5bebb794cac83aa3af
ohmink <kkop5056@naver.com>
2020-12-15 01:16:42 +0900

    refactor: 테스트 코드 삭제

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 import App from './App';
-import Test from './Test';
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
-    <Route path="/test" component={Test} exact />
   </BrowserRouter>,
   document.getElementById('root')
 );
