4f189fd7cd68f5f196bf87aa52d873e54432af88
ohmink <kkop5056@naver.com>
2020-12-15 01:15:58 +0900

    fix: 헤더 중복 해결 헤더가 중복되어 색이 진해지던 문제를 수정

--- src/pages/IssueDetail.tsx
+++ src/pages/IssueDetail.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Toolbar from '@material-ui/core/Toolbar';
-
-import Header from '@components/utils/Header';
 import LeftBar from '@components/Issues/LeftBar';
 import MainContainer from '@components/Issues/detail/MainContainer';
 
 const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    backgroundColor: 'rgba(0,0,0,0.3)',
   },
   content: {
     flexGrow: 1,
@@ -24,16 +17,11 @@ const useStyles = makeStyles(theme => ({
 
 const MainPage = ({ match }) => {
   const classes = useStyles();
-  console.log(match);
-  console.log(match.params);
   const { issueId } = match.params;
-  console.log(issueId);
+
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <AppBar position="fixed" className={classes.appBar}>
-        <Header />
-      </AppBar>
       <LeftBar />
 
       <main className={classes.content}>
