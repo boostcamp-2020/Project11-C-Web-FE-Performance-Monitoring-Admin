9b6c6c640810ec277d130199d9337cdf6896df22
Jeesoo Ha <haajeesoo@gmail.com>
2020-12-17 00:46:18 +0900

    Revert "통계 화면 디자인, 사이드바 기능 추가 구현, 설명 화면 수정"

--- src/utils/Interface.ts
+++
@@ -1,5 +0,0 @@
-export interface CreateProject {
-  title: string;
-  description: string;
-  platform: string;
-}
--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,43 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
-const webpack = require('webpack');
-const dotenv = require('dotenv');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = () => {
-  dotenv.config({ path: './.env' }).parsed;
-
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/index',
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/index.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
     },
-    resolve: {
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      alias: {
-        '@components': path.resolve(__dirname, 'src/components'),
-        '@pages': path.resolve(__dirname, 'src/pages'),
-        '@molecules': path.resolve(__dirname, 'src/molecules'),
-        '@utils': path.resolve(__dirname, 'src/utils'),
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: '/node_modules/',
+        loader: 'babel-loader',
       },
-    },
-    output: {
-      path: path.resolve(__dirname, './dist'),
-      filename: 'bundle.js',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx|ts|tsx)$/,
-          exclude: /node_modules/,
-          use: [{ loader: 'babel-loader' }, { loader: 'ts-loader' }],
-        },
-        {
-          test: /\.css$/,
-          use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
-        },
-        {
-          test: /\.(png|jpe?g|gif|svg)$/i,
-          use: [{ loader: 'file-loader' }],
-        },
-      ],
-    },
-    devServer: {
-      historyApiFallback: true,
-      port: 8000,
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './public/index.html',
-      }),
-      new CleanWebpackPlugin(),
-      new webpack.ProvidePlugin({
-        process: 'process/browser',
-      }),
-      new webpack.DefinePlugin({
-        'process.env.API_URL': JSON.stringify(process.env.API_URL),
-      }),
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+      },
     ],
-  };
+  },
+  devServer: {
+    historyApiFallback: true,
+    port: 3000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
 };
